a8ace0439a308151050494abfbb6ad3fc826ade9
Tyler Spens <tyler.spens@venafi.com>
2019-12-11 11:58:42 -0700

    Added Discovery WebSDK. Also fixed SecretStore API method.

--- venafi/properties/response_objects/identity.py
+++ venafi/properties/response_objects/identity.py
@@ -1,20 +1,25 @@
 class Identity:
     class Identity:
-        def __init__(self, identity_dict, api_type):
-            """
-            :type identity_dict: dict
-            :type api_type: str
-            """
-            if api_type.lower() == 'websdk':
-                self.FullName = identity_dict.get('FullName')
-                self.IsContainer = identity_dict.get('IsContainer')
-                self.IsGroup = identity_dict.get('IsGroup')
-                self.Name = identity_dict.get('Name')
-                self.Prefix = identity_dict.get('Prefix')
-                self.PrefixedName = identity_dict.get('PrefixedName')
-                self.PrefixedUniversal = identity_dict.get('PrefixedUniversal')
-                self.Universal = identity_dict.get('Universal')
+        def __init__(self, identity_dict: dict):
+            if not isinstance(identity_dict, dict):
+                identity_dict = {}
 
-            elif api_type.lower() == 'aperture':
-                # Not implemented yet.
-                pass
+            self.full_name = identity_dict.get('FullName')
+            self.is_container = identity_dict.get('IsContainer')
+            self.is_group = identity_dict.get('IsGroup')
+            self.name = identity_dict.get('Name')
+            self.prefix = identity_dict.get('Prefix')
+            self.prefixed_name = identity_dict.get('PrefixedName')
+            self.prefixed_universal = identity_dict.get('PrefixedUniversal')
+            self.type = identity_dict.get('Type')
+            self.universal = identity_dict.get('Universal')
+
+    class InvalidMembers:
+        def __init__(self, invalid_members_dict: dict):
+            if not isinstance(invalid_members_dict, dict):
+                invalid_members_dict = {}
+
+            self.prefix = invalid_members_dict.get('Prefix')
+            self.prefixed_name = invalid_members_dict.get('PrefixedName')
+            self.prefixed_universal = invalid_members_dict.get('PrefixedUniversal')
+            self.universal = invalid_members_dict.get('Universal')
