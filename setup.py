aa63c443e81bce17605d140909174ba739ad6313
Tyler Spens <tyler.spens@venafi.com>
2020-02-11 15:54:31 -0700

    - Updated requirements file.

--- setup.py
+++ setup.py
@@ -1,21 +1,24 @@
+__version__ = "19.4.0.0"
+
 from setuptools import setup, find_packages
 
-setup(
-    name='venafi',
-    packages=find_packages(where='venafi', exclude=['docs']),
-    package_dir={
-      '': 'venafi'
-    },
-    description='Venafi Features and API Implementation In Python',
-    version='19.4.0.0',
-    url='ssh://git@git.eng.venafi.com/spi',
-    author='Venafi',
-    author_email='spi@venafi.com',
-    keywords=['pip','venafi'],
-    install_requires=[
-        'requests',
-        'datetime',
-        'jsonpickle',
-        'python-dateutil'
-    ],
-)
+if __name__ == '__main__':
+    setup(
+        name='venafi',
+        packages=find_packages(where='venafi', exclude=['docs']),
+        package_dir={
+          '': 'venafi'
+        },
+        description='Venafi Features and API Implementation In Python',
+        version=__version__,
+        url='ssh://git@git.eng.venafi.com/spi',
+        author='Venafi',
+        author_email='spi@venafi.com',
+        keywords=['pip','venafi'],
+        install_requires=[
+            'requests',
+            'datetime',
+            'jsonpickle',
+            'python-dateutil'
+        ],
+    )
