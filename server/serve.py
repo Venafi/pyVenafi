d6581091b0ffcab8c870e859057a2a8b8740633f
Tyler Spens <tyler.spens@venafi.com>
2021-02-16 09:37:59 -0700

    - Updated API/documentation flask service.

--- server/serve.py
+++ server/serve.py
@@ -5,52 +5,33 @@ from pathlib import Path
 
 PACKAGES_PATH = os.path.abspath(r'C:\SPI\spi\packages')
 DOCS_PATH = os.path.abspath(r'C:\SPI\spi\docs')
-LATEST_DOC_VERSION = sorted(os.listdir(DOCS_PATH))[-1]
 app = flask.Flask(__name__, static_folder=DOCS_PATH)
 
 
 @app.route('/spi')
 @app.route('/spi/docs')
 def default_to_versions():
-    print(f'redirecting to /spi/docs/{LATEST_DOC_VERSION}')
-    return flask.redirect(f'/spi/docs/{LATEST_DOC_VERSION}')
+    print(f'redirecting to /spi/docs')
+    return flask.redirect(f'/spi/docs/index.html', code=302)
 
 
-@app.route('/spi/api/latestVersion', methods=['GET'])
-def get_latest_version():
-    base_version = flask.request.args.get('baseVersion')
-    components = []
-    if not base_version or not len(components := base_version.split('.')) == 4:
-        avail_versions = glob.glob(
-            f'{PACKAGES_PATH}/*'
-        )
-        if not avail_versions:
-            return flask.abort(400, 'No packages available.')
-    else:
-        adjusted_comps = ".".join(components[:-1])
-        avail_versions = glob.glob(
-            f'{PACKAGES_PATH}/venafi-{adjusted_comps}*'
-        )
-        if not avail_versions:
-            return flask.abort(400, f'No packages available for {adjusted_comps}*.')
-    latest = sorted(avail_versions)[-1]
-    return os.path.basename(latest.replace('venafi-', '').replace('.tar.gz', ''))
-
-
-@app.route('/spi/docs/<version>')
-def default_to_version_index(version=LATEST_DOC_VERSION):
-    print(f'redirecting to /spi/docs/{version}/versions.html')
-    return flask.redirect(f'/spi/docs/{version}/versions.html')
-
-
-@app.route('/spi/docs/<version>/<path:path>')
-def docs(version=LATEST_DOC_VERSION, path='index.html'):
-    path = Path(f'{DOCS_PATH}\\{version}\\html\\{path}')
-    print(path)
+@app.route('/spi/docs/<path:path>')
+def docs(path='index.html'):
+    path = Path(f'{DOCS_PATH}\\html\\{path}')
     return flask.send_from_directory(
         directory=path.parent,
         filename=path.name
     )
+
+
+@app.route('/spi/api/latestVersion', methods=['GET'])
+def get_latest_version():
+    get_version_from_filename = lambda x: os.path.basename(x.replace('venafi-', '').replace('.tar.gz', ''))
+    all_versions = glob.glob(f'{PACKAGES_PATH}/*')
+    if not all_versions:
+        return flask.abort(400, 'No packages available.')
+    latest_version = get_version_from_filename(sorted(all_versions)[-1])
+    return flask.jsonify({'latestVersion': latest_version})
 
 
 if __name__ == '__main__':
