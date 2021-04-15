26762063ed0921e3b77a1c1fa6b994c85995b5fe
Tyler Spens <tyler.spens@venafi.com>
2021-04-15 09:26:02 -0600

    - Moved docs.

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../venafi'))
 
 
 # -- Project information -----------------------------------------------------
@@ -47,8 +47,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'all_docs/excluded/*.rst
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_logo = os.path.abspath('./_static/logo.png')
-html_favicon = os.path.abspath('./_static/favicon.png')
+html_logo = os.path.abspath('_static/logo.png')
+html_favicon = os.path.abspath('_static/favicon.png')
 
 html_theme = "sphinx_rtd_theme"
 html_theme_path = ["_themes", ]
