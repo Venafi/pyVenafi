01947a060ecd4583209d0ab9e6e1d11fcbf56b8e
Tyler Spens <tyler.spens@venafi.com>
2022-02-04 14:03:42 -0700

    - Modified setup strategy.

--- setup.py
+++ setup.py
@@ -9,17 +9,31 @@ exec(open('pytpp/_about.py', 'r').read())
 from setuptools import setup, find_packages
 import os
 
+PROD_REQUIREMENTS = [
+    'isodate==0.6.0',
+    'logboss~=0.1.8',
+    'packaging==20.9',
+    'python-dateutil==2.8.2',
+    'requests==2.24.0',
+]
+
+DEV_REQUIREMENTS = [
+    'pyodbc==4.0.30',
+    'lxml==4.4.1',
+    'pandas==1.3.3',
+    'paramiko==2.7.1'
+]
+
+DOC_REQUIREMENTS = [
+    'Sphinx>=4.3.2',
+    'sphinxcontrib-napoleon~=0.7',
+    'sphinx-rtd-theme~=1.0.0',
+    'sphinx-rtd-dark-mode~=1.2.4',
+]
 
 if __name__ == '__main__':
     with open(os.path.join(os.path.dirname(__file__), 'README.md')) as f:
         long_description = f.read()
-
-    if os.path.exists('requirements/prod.txt'):
-        with open('requirements/prod.txt', 'r') as f:
-            requirements = f.readlines()
-    else:
-        with open('requirements.txt', 'r') as f:
-            requirements = f.readlines()
 
     setup(
         name=__project_name__,
@@ -35,7 +49,12 @@ if __name__ == '__main__':
         long_description=long_description,
         long_description_content_type='text/markdown',
         keywords=['pytpp', 'venafi', 'tpp', 'trust protection platform'],
-        install_requires=requirements,
+        install_requires=PROD_REQUIREMENTS,
+        extras_require={
+            'dev': DEV_REQUIREMENTS,
+            'doc': DOC_REQUIREMENTS,
+            'all': DOC_REQUIREMENTS + DEV_REQUIREMENTS
+        },
         classifiers=[
             'Development Status :: 4 - Beta',
             'Intended Audience :: Developers',
