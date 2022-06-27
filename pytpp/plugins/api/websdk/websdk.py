c88f59823f58a4d37a31147267bc179e7983828e
Tyler Spens <tyler.spens@venafi.com>
2022-06-27 15:07:04 -0600

    - Missed WebSDK plugins class for adding connection and read timeouts. - Pushed 1.1.3.

--- pytpp/plugins/api/websdk/websdk.py
+++ pytpp/plugins/api/websdk/websdk.py
@@ -3,12 +3,6 @@ from pytpp.plugins.api.websdk.endpoints.rights import _Rights
 
 
 class WebSDK(WebSDK):
-    def __init__(self, host: str, username: str, password: str, token: str = None, application_id: str = None,
-                 scope: Union[Scope, str] = None, refresh_token: str = None, proxies: dict = None,
-                 certificate_path: str = None, key_file_path: str = None, verify_ssl: bool = False):
-        super().__init__(
-            host=host, username=username, password=password, token=token, application_id=application_id,
-            scope=scope, refresh_token=refresh_token, proxies=proxies, certificate_path=certificate_path,
-            key_file_path=key_file_path, verify_ssl=verify_ssl
-        )
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.Rights = _Rights(self)
