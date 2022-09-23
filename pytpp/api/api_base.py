3e34c22703e95ce2566db7fef54cdb1e7ef6f713
Tyler Spens <tyler.spens@venafi.com>
2022-09-23 12:20:50 -0600

    - Fixed bugs in asserting and validating responses within the features. - Fixed bug in writing name/attribute values with config by casting all values as strings (as before pytpp 2.0.0) - Pushed 2.0.6.

--- pytpp/api/api_base.py
+++ pytpp/api/api_base.py
@@ -304,7 +304,7 @@ def generate_output(response: Response, output_cls: Type[T_], root_field: str = 
         result = {}
     if not isinstance(result, dict):
         if not root_field:
-            raise AttributeError('Unable to assign ')
+            raise AttributeError('Expected a root_field, but no root_field is defined.')
         result = {
             str(root_field): result
         }
@@ -386,5 +386,5 @@ class WebSdkOutputModel(RootOutputModel):
 class InvalidResponseError(Exception):
     def __init__(self, msg: str, response: Response):
         self.response = response
-        super().__init__(msg)
+        super().__init__(msg, response)
 # endregion Response Definitions
